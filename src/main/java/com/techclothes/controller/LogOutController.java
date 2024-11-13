369a9bfa135df37966c292ecfaf08db1d8499df3
gabrielsor26 <gabriel.vilchez.chota@gmail.com>
2024-11-13 14:57:14 -0500

    test

--- src/main/java/com/techclothes/controller/LogOutController.java
+++ src/main/java/com/techclothes/controller/LogOutController.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
-
+//test
 @WebServlet("/LogOutController")
 public class LogOutController extends HttpServlet {
 
